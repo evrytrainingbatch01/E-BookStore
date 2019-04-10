7c802308c4bcf8005b0ea3493afc0df8cfe4f43e
Venkata Surya Kiran Kuppili <venkata.kuppili@evry.com>
2019-04-10 12:51:47 +0530

    Controller Code is updated as per project structure.  -- venkat

--- src/main/java/com/evry/ebookstore/controllerimpl/BookControllerImpl.java
+++ src/main/java/com/evry/ebookstore/controllerimpl/BookControllerImpl.java
@@ -1,5 +1,50 @@
 package com.evry.ebookstore.controllerimpl;
 
-public class BookControllerImpl {
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.evry.ebookstore.controller.BookController;
+import com.evry.ebookstore.model.BookEntity;
+import com.evry.ebookstore.service.BookService;
+
+/**
+ * 
+ * @author venkata.kuppili
+ *
+ */
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class BookControllerImpl implements BookController{
+
+	/** Injecting BookService classs into controller */
+	@Autowired
+	public BookService bookService;
+
+	/**
+	 * This method is used for get all the books info.
+	 * 
+	 * @return List<BookEntity>
+	 */
+	@GetMapping("/books")
+	public List<BookEntity> getAllBooks() {
+		return bookService.getAllBooks();
+	}
+
+	/**
+	 * This method get the array of books and return total prise of the books.
+	 * 
+	 * @param books List<Map<String, BookEntity>>.
+	 * @return price double.
+	 */
+	@GetMapping("/books/price")
+	public double buyBooks(@RequestBody final List<Map<String, BookEntity>> books) {
+		return bookService.buyBooks(books);
+	}
 
 }
