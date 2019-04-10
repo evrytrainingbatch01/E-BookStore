fe624a12701a2b2d6d52b5d756edb42198469a37
evrytrainingbatch01 <48678186+evrytrainingbatch01@users.noreply.github.com>
2019-04-10 17:12:54 +0530

    added new method by manoj

--- src/main/java/com/evry/ebookstore/service/BookService.java
+++ src/main/java/com/evry/ebookstore/service/BookService.java
@@ -1,11 +1,14 @@
 package com.evry.ebookstore.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.evry.ebookstore.model.BookEntity;
 
 public interface BookService {
 	
 	public List<BookEntity> getAllBooks();
+
+	public double buyBooks(List<Map<String, Object>> selectedBooks);
 
 }
