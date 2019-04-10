e44389d598d17b31b0c168f8cb1fbfd1c7d0afff
evrytrainingbatch01 <evrytrainingbatch01@gmail.com>
2019-04-10 16:01:42 +0530

    implemented the getAllBooks() method in service layer by Srinivasa Rao

--- src/main/java/com/evry/ebookstore/service/BookService.java
+++ src/main/java/com/evry/ebookstore/service/BookService.java
@@ -1,5 +1,11 @@
 package com.evry.ebookstore.service;
 
+import java.util.List;
+
+import com.evry.ebookstore.model.BookEntity;
+
 public interface BookService {
+	
+	public List<BookEntity> getAllBooks();
 
 }
--- src/main/java/com/evry/ebookstore/serviceimpl/BookServiceImpl.java
+++ src/main/java/com/evry/ebookstore/serviceimpl/BookServiceImpl.java
@@ -1,9 +1,41 @@
 package com.evry.ebookstore.serviceimpl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.evry.ebookstore.dao.BookDAO;
+import com.evry.ebookstore.model.BookEntity;
 import com.evry.ebookstore.service.BookService;
+
+/**
+ * 
+ * @author srinivasa.nayana
+ * 
+ * class BookServiceImpl implementing the interface BookService to implement the methods.
+ *
+ */
 @Service
 public class BookServiceImpl implements BookService{
+
+	/**
+	 * Below statement is to have BookDAOImpl object injected into BookServiceImpl class by spring container implicitly.
+	 * this is achieved by @Autowired annotation 
+	 */
+	@Autowired
+	private BookDAO bookDao;
+	
+	
+	/**
+	 * @getAllBooks() method is used here to get the list of books by calling getAllBooks() of BookDAOImpl class of Dao layer.
+	 * @return List<BookEntity> (list of objects of BookEntity type) 
+	 */
+	public List<BookEntity> getAllBooks() {
+		
+		return bookDao.getAllBooks();
+	}
+	
+
 
 }
